f6d076e3588beb26f7ac8c5aa94a8b7c542dbb26
dpace11 <kayasthadeepesh7@gmail.com>
2023-06-05 16:09:51 +0545

    implementation of sum method in DayOne class

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace ConsoleApp1;
+
+public class DayOne
+{
+    /// <summary>
+    /// Create a function that takes two numbers as arguments and returns their sum.
+    /// </summary>
+    /// <example>
+    /// addition(3, 2) ➞ 5
+    /// addition(-3, -6) ➞ -9
+    /// </example>
+
+    public void Sum(int firstnum, int secondnum)
+    {
+        var result = firstnum + secondnum;
+        Console.WriteLine($"the sum of {firstnum} and {secondnum} is {result}");
+
+    }
+}
