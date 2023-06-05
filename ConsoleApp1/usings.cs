1bff3448606e3955dd568aa6ebc26fd902c5cdfb
dpace11 <kayasthadeepesh7@gmail.com>
2023-06-05 16:05:40 +0545

    create a using class containing all using statements

---
+++ ConsoleApp1/usings.cs
@@ -0,0 +1,7 @@
+﻿global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System;
+global using System.Collections.Generic;
+
+global using ConsoleApp1;
