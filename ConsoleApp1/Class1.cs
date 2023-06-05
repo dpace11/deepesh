c029989d13ebba7eb6dd86d9722274aba9ec8aa0
dpace11 <kayasthadeepesh7@gmail.com>
2023-06-05 22:14:19 +0545

    calculation of remainder

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -80,5 +80,17 @@ public class DayOne
         var area = (length * breadth) / 2;
         Console.WriteLine($"area of triangle of length:{length} & breadth{breadth} is {area}");
     }
-    
+
+    /// <summary>
+    /// There is a single operator in C#, capable of providing the remainder of a division operation. 
+    /// Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+    /// </summary>
+   
+    public void Rem(int num1, int num2)
+    {
+        var result = num1 % num2;
+        Console.WriteLine($"remainder of {num1} & {num2} is {result}");
+
+
+    }
 }
