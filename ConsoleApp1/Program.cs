4c45536253c759bef6fb0cc3af85bedb37a8e665
dpace11 <kayasthadeepesh7@gmail.com>
2023-06-05 21:26:59 +0545

    age to days conversion
    accept age from user and convert into corresponding age

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,6 +9,11 @@ Console.WriteLine();
 dayOne.NextNumber(5);
 Console.WriteLine();
 
+dayOne.PowerCircuitCalcu(15, 40);
+Console.WriteLine();
 
+Console.WriteLine("enter your age");
+int x=int.Parse(Console.ReadLine());
+dayOne.AgeToDays(x);
 
 
